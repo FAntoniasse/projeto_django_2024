58cd222bdedcee8de9ea6d74719f705b62093110
Fabrício Costa Antoniasse <fabricio.antoniasse@hotmail.com>
2024-10-07 10:01:40 -0300

    search form

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-def sobre(request):
-    return HttpResponse("SOBRE")
-
-def contato(request):
-    return HttpResponse("CONTATO")
+    return render(request, 'recipes/pages/home.html', context={
+        'name' : 'Fabrício Antoniasse'
+    })
